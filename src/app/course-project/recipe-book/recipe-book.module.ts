4bd246e4489813852aa599651accdb83879406b8
Enoch Offiah <enochoffiah@yahoo.co.uk>
2022-03-16 15:50:11 +0000

    course project structure, add recipe list component

--- src/app/course-project/recipe-book/recipe-book.module.ts
+++ src/app/course-project/recipe-book/recipe-book.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RecipeBookComponent } from './recipe-book.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [RecipeBookComponent, RecipeDetailComponent, RecipeItemComponent],
-  exports:[RecipeBookComponent, RecipeDetailComponent, RecipeItemComponent]
+  declarations: [RecipeBookComponent, RecipeDetailComponent, RecipeItemComponent, RecipeListComponent],
+  exports:[RecipeBookComponent, RecipeDetailComponent, RecipeItemComponent, RecipeListComponent]
 })
 export class RecipeBookModule { }
