2138fd9514a6ed2551d758d92c0e1e596b984d73
Enoch Offiah <enochoffiah@yahoo.co.uk>
2022-03-17 08:39:29 +0000

    reorganised project folder structure

--- src/app/course-project/recipe-book/recipe-book.module.ts
+++ src/app/course-project/recipe-book/recipe-book.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeBookComponent } from './recipe-book.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [RecipeBookComponent, RecipeDetailComponent, RecipeItemComponent, RecipeListComponent],
-  exports:[RecipeBookComponent, RecipeDetailComponent, RecipeItemComponent, RecipeListComponent]
+  declarations: [RecipeBookComponent, RecipeDetailComponent, RecipeListComponent, RecipeItemComponent],
+  exports:[RecipeBookComponent, RecipeDetailComponent, RecipeListComponent, RecipeItemComponent]
 })
 export class RecipeBookModule { }
